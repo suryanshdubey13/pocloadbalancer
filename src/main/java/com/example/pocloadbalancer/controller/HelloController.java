759f49bb7edb631aa90754738b20be3160900297
suryanshdubey13 <dubeysuryansh@gmail.com>
2026-02-19 00:54:41 +0530

    pocLoadBalancer app update

--- src/main/java/com/example/pocloadbalancer/controller/HelloController.java
+++ src/main/java/com/example/pocloadbalancer/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Hello")
+@RequestMapping("/hello")
 public class HelloController {
 
     @Value("${HOSTNAME:unknown}")
