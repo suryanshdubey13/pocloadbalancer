98081932a93f2dee79b9e752e3428309f6e94679
suryanshdubey13 <dubeysuryansh@gmail.com>
2026-02-23 14:52:49 +0530

    Update HelloController.java

--- src/main/java/com/example/pocloadbalancer/controller/HelloController.java
+++ src/main/java/com/example/pocloadbalancer/controller/HelloController.java
@@ -1,17 +1,19 @@
 package com.example.pocloadbalancer.controller;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/service")
     public String hello() {
-          return "Response from container: " +
-                System.getenv("HOSTNAME");
+          return "Hello from port: " + serverPort;
     }
 }
